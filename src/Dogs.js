f77f9ecd26502970026e381858335e1c0bb06167
Abdyrahmanov Timurlan <timurlanabdyrahmanov7@gmail.com>
2021-08-23 11:59:54 +0600

    first commit

---
+++ src/Dogs.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+const Dogs = () => {
+    const [breed,setBreed] = useState([])
+    return (
+        <div>
+            <select name="" id="">
+                <option value=""> </option>
+            </select>
+        </div>
+    );
+};
+
+export default Dogs;
---
+++ src/Starwars.js
@@ -0,0 +1,20 @@
+// import React, {useState} from 'react';
+//
+// const Starwars = () => {
+//     const [userId,setUserId] = useState('')
+//     const [people,setPeople] = useState({})
+//     const handleInput = (e)=>{
+//      setUserId(e.target.value)
+//     }
+//     const handleClick = ()=>{
+//
+//     }
+//     return (
+//         <div>
+//             <input type="text" onChange={handleInput}/>
+//             <button onClick={handleClick}>Get</button>
+//         </div>
+//     );
+// };
+//
+// export default Starwars;
